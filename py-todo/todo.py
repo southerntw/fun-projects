3e7a54f18579b5e1fb9b6dd99b0a63a14d6bd213
o-jan <fauzanmansur.a@gmail.com>
2021-11-18 15:57:54 +0800

    what functions?

--- py-todo/todo.py
+++ py-todo/todo.py
@@ -1,50 +1,11 @@
 #!/usr/bin/env python
-import sys
-import argparse
+import args
 
 file_name = "list.txt"
 file = open(file_name, 'a+')
 
-def import_file():
-  with open(file_name) as f:
-    li = f.read().splitlines()
-  return li
+with open(file_name) as f:
+  li = f.read().splitlines()
 
-def arguments():
-  # help messages
-  msg = 'Simple and maybe useless to do list made from python'
-  add = 'Add task to your to do list'
-  
-  # arguments parser
-  parser = argparse.ArgumentParser(description = msg)
-  parser.add_argument('-a', '-add', dest='task', help=add)
-  args = vars(parser.parse_args())
-
-  return args
-
-def process(args):
-  li = import_file()
-
-  # initialize stuffs
-  add = args['task']
-
-  # append the todo list
-  if add:
-    append(li, add)
-
-def append(li, add):
-  try:
-    file.write(add + "\n")
-    print("Completed.")
-  except:
-   print("An error occured.")
-
-def printList(li):
-  for i in li:
-    print(i)
-
-if __name__ == "__main__":
-  args = arguments()
-  li = import_file()
-  process(args)
-  printList(li)
+for i in li:
+  print(i)
